61dfa1dad6b70fabedba0ec334293145620124cf
Rafael Santiago <voidbrainvoid@gmail.com>
2015-04-28 13:08:40 -0300

    The to_str() function must receives a literal string (i.e.: "...")

--- src/to_str.c
+++ src/to_str.c
@@ -6,15 +6,17 @@
 #define getnibv(n) ( isxdigit((n)) ? toupper((n)) - 55 : (n) - 48 )
 
 char *to_str(const char *value) {
-    const char *vp = value;
+    const char *vp = value, *vp_end = NULL;
     char *retval = NULL, *rp = NULL;
     unsigned char byte = 0;
     if (vp == NULL) {
 	return NULL;
-    }
+    }    
     retval = pig_newseg(strlen(value) + 1);
     memset(retval, 0, strlen(value) + 1);
     rp = retval;
+    vp += 1;
+    vp_end = value + strlen(value) - 1;
     while (*vp != 0) {
 	if (*vp == '\\') {
 	    vp++;
