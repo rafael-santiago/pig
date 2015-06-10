a011f160d5e895b9e71d243e93592a389ce36844
rafael-santiago <voidbrainvoid@gmail.com>
2015-06-10 15:22:23 -0300

    GPL/Copyright disclaimer

--- src/linux/rsk.c
+++ src/linux/rsk.c
@@ -1,3 +1,10 @@
+/*
+ *                                Copyright (C) 2015 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
 #include "rsk.h"
 #include <unistd.h>
 #include <sys/types.h>
