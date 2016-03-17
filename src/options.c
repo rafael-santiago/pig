5a6384235a6c7032ed8299798fe59c0b906da371
rafael-santiago <voidbrainvoid@gmail.com>
2016-03-17 10:32:03 -0300

    The GPL disclaimer that was lacking

--- src/options.c
+++ src/options.c
@@ -1,3 +1,10 @@
+/*
+ *                                Copyright (C) 2016 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
 #include "options.h"
 #include <string.h>
 
